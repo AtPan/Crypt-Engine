702844a9a7d3c1c0c3162b09e21a12f59d54a3f5
Anthony Panarello <jpk8g2@gmail.com>
2023-01-20 12:08:11 -0500

    Added function prototype for Crypt_init_memory and Crypt_alloc

--- include/Crypt_utils/memory.h
+++ include/Crypt_utils/memory.h
@@ -18,13 +18,15 @@
 
 #include <stddef.h>
 #include "definitions.h"
+#include "memory/structs.h"
 
 #define __CRYPT_DEFAULT_BLOCK_SIZE 128
-#define __CRYPT_DEFAULT_MEMORY_ALLOCATION 1 << 16
+#define __CRYPT_DEFAULT_MEMORY_ALLOCATION 1 << 8
 #define __CRYPT_ROUND_TO_NEXT_BLOCK(x) ((x) + (sizeof(struct __memory_block) - ((size_t)(x) % sizeof(struct __memory_block))) % sizeof(struct __memory_block))
 #define __CRYPT_POINTER_AS_INT(x) (size_t)(x)
 #define __CRYPT_INT_AS_POINTER(x, type) (type *)(x)
 
-FLAG Crypt_init_memory(size_t n);
+FLAG Crypt_init_memory(size_t);
+void * Crypt_alloc(size_t);
 
 #endif
