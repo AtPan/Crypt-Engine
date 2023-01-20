431ac596cedf2786bda357c59aa0c7b7effcce27
Anthony Panarello <jpk8g2@gmail.com>
2023-01-20 12:03:02 -0500

    Creates a new memory block, marks the old as reserved, and returns the data segment of the old

--- src/memory/mem_alloc.c
+++ src/memory/mem_alloc.c
@@ -20,9 +20,10 @@ extern struct __memory __memory_buf;
 
 void * Crypt_alloc(size_t n) {
     if(n == 0) return NULL;
+
     n = __CRYPT_ROUND_TO_NEXT_BLOCK(n);
 
-    if(n < __memory_buf.allocated) {
+    if(n > __memory_buf.allocated - __memory_buf.size) {
         /* TODO: Expand allocated buffer
          */
 
@@ -30,7 +31,24 @@ void * Crypt_alloc(size_t n) {
     }
 
     struct __memory_block * block = (struct __memory_block *)__memory_buf.buf;
-    while(block != NULL && block->allocated < n) block = block->next;
 
-    if(block == NULL) return NULL;
+    while(block != NULL && block->block_size < n) {
+        block = block->next;
+    }
+
+    if(block == NULL || block->block_size < n) return NULL;
+
+    block->is_allocated = TRUE;
+
+    if(block->block_size != n) {
+        struct __memory_block * new_block = (struct __memory_block *)((size_t)block + sizeof(struct __memory_block) + n);
+        new_block->is_allocated = FALSE;
+        new_block->block_size = block->block_size - n;
+        new_block->next = block->next;
+
+        block->block_size = n;
+        block->next = new_block;
+    }
+
+    return (void *)((size_t)block + sizeof(struct __memory_block));
 }
