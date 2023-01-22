2294d76cf674945bdd56e04339fa901f75fe9306
Anthony Panarello <jpk8g2@gmail.com>
2023-01-22 10:48:26 -0500

    Return freed memory back to the pool

---
+++ src/memory/mem_free.c
@@ -0,0 +1,42 @@
+/*  This file is part of Crypt Engine.
+ *
+ *  Crypt Engine is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  Crypt Engine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with Crypt Engine.
+ *  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <Crypt_utils/memory.h>
+#include <Crypt_utils/memory/structs.h>
+
+extern struct __memory __memory_buf;
+
+void Crypt_free(void *ptr) {
+    size_t addr = (size_t)ptr;
+    struct __memory_block * block = (struct __memory_block *)__memory_buf.buf;
+    struct __memory_block * parent = block;
+
+    if(addr < (size_t)__memory_buf.buf || addr > (size_t)__memory_buf.buf + __memory_buf.allocated) return;
+
+    while(block != NULL && addr > (size_t)block) {
+        parent = block;
+        block = block->next;
+    }
+
+    if(parent->is_allocated == FALSE) { /* wtf ??? */ return; }
+
+    parent->is_allocated = FALSE;
+
+    __memory_buf.size -= parent->block_size;
+
+    if(block != NULL && block->is_allocated == FALSE) {
+        parent->block_size += block->block_size;
+        parent->next = block->next;
+    }
+}
