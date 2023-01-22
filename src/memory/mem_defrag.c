2da8cdb6773ce7657306547f957a2d90cc8ffb19
Anthony Panarello <jpk8g2@gmail.com>
2023-01-22 10:48:46 -0500

    A simple function that defragments the memory pool

---
+++ src/memory/mem_defrag.c
@@ -0,0 +1,31 @@
+/*  This file is part of Crypt Engine.
+ *
+ *  Crypt Engine is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  Crypt Engine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with Crypt Engine.
+ *  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <Crypt_utils/memory.h>
+#include <Crypt_utils/memory/structs.h>
+
+extern struct __memory __memory_buf;
+
+void Crypt_defrag_memory() {
+    struct __memory_block * block = (struct __memory_block *)__memory_buf.buf;
+
+    while(block->next != NULL) {
+        while(block->next != NULL && block->is_allocated == TRUE) block = block->next;
+
+        while(block->next != NULL && block->next->is_allocated == FALSE) {
+            block->block_size += block->next->block_size;
+            block->next = block->next->next;
+        }
+    }
+}
