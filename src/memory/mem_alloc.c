5bccb5ecc20475df76b9b9d70e052f778185f2c7
Anthony Panarello <jpk8g2@gmail.com>
2023-01-22 10:56:31 -0500

    Auto-defrags memory in case of failure to allocate and tries again

--- src/memory/mem_alloc.c
+++ src/memory/mem_alloc.c
@@ -19,9 +19,10 @@
 extern struct __memory __memory_buf;
 
 void * Crypt_alloc(size_t n) {
+    void * __Crypt_mem_alloc_helper(size_t);
     if(n == 0) return NULL;
 
-    n = __CRYPT_ROUND_TO_NEXT_BLOCK(n);
+    n = __CRYPT_ROUND_TO_NEXT_BLOCK(n + sizeof(struct __memory_block));
 
     if(n > __memory_buf.allocated - __memory_buf.size) {
         /* TODO: Expand allocated buffer
@@ -30,18 +31,29 @@ void * Crypt_alloc(size_t n) {
         return NULL;
     }
 
+    void * mem;
+
+    if((mem = __Crypt_mem_alloc_helper(n)) == NULL) {
+        Crypt_defrag_memory();
+        mem = __Crypt_mem_alloc_helper(n);
+    }
+
+    return mem;
+}
+
+void * __Crypt_mem_alloc_helper(size_t n) {
     struct __memory_block * block = (struct __memory_block *)__memory_buf.buf;
 
     while(block != NULL && (block->is_allocated == TRUE || block->block_size < n)) {
         block = block->next;
     }
 
-    if(block == NULL || block->block_size < n) return NULL;
+    if(block == NULL || block->block_size < n || block->is_allocated == TRUE) return NULL;
 
     block->is_allocated = TRUE;
 
     if(block->block_size != n) {
-        struct __memory_block * new_block = (struct __memory_block *)((size_t)block + sizeof(struct __memory_block) + n);
+        struct __memory_block * new_block = (struct __memory_block *)((size_t)block + n);
         new_block->is_allocated = FALSE;
         new_block->block_size = block->block_size - n;
         new_block->next = block->next;
