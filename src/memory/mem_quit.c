343da3f5689df46a17e1f52301ac05542d683bc3
Anthony Panarello <aPanarello20@gmail.com>
2023-01-30 15:02:53 -0500

    Added quit function that frees and stops the memory module

---
+++ src/memory/mem_quit.c
@@ -0,0 +1,36 @@
+/*  This file is part of Crypt Engine.
+ *
+ *  Crypt Engine is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  Crypt Engine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with Crypt Engine.
+ *  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <Crypt_utils/memory.h>
+#include <Crypt_utils/memory/structs.h>
+#include <stdlib.h>
+
+extern struct __memory __memory_buf;
+
+/* Crypt_quit_memory
+ * --------------------------------------
+ *  Frees all allocated memory from the pool and
+ *  resets the master memory struct.
+ *
+ *  DO NOT use any pointers allocated from Crypt_alloc or Crypt_realloc,
+ *  call Crypt_alloc, Crypt_realloc, Crypt_free, Crypt_defrag_memory, or any other
+ *  Crypt memory function other than Crypt_init_memory after calling this function.
+ */
+void Crypt_quit_memory() {
+    if(__memory_buf.buf == NULL) return;
+
+    free(__memory_buf.buf);
+    __memory_buf.buf = NULL;
+    __memory_buf.allocated = __memory_buf.size = 0;
+}
