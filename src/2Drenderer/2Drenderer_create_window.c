1fea1cac4d83fa0a911b498fcff5c790dc576e0e
Anthony Panarello <aPanarello20@gmail.com>
2023-04-14 11:32:44 -0400

    Refactored Crypt_2D_renderer_create_window to create and store more items for quick access when rendering to the SDL objects, with error checking and resource cleanup

--- src/2Drenderer/2Drenderer_create_window.c
+++ src/2Drenderer/2Drenderer_create_window.c
@@ -14,20 +14,56 @@
  *  Copyright 2023 Anthony Panarello
  */
 
+#include <SDL_render.h>
 #include <Crypt_renderer.h>
 
-flag_t Crypt_2D_renderer_create_window(Crypt_2DWindow_t ** restrict window, Crypt_2DWindow_opts_t window_opts) {
+flag_t Crypt_2D_renderer_create_window(Crypt_2DWindow_t * restrict window, Crypt_2DWindow_opts_t window_opts) {
     if(window == NULL) return FAIL;
 
-    Crypt_2DWindow_t * l_window = SDL_CreateWindow(window_opts.name,
-            window_opts.x, window_opts.y, window_opts.w, window_opts.h, window_opts.flags);
+    window->background_color = window_opts.default_color;
 
-    if((*window = l_window) == NULL) {
+    SDL_Window * sdl_w = SDL_CreateWindow(window_opts.name,
+            window_opts.x, window_opts.y, window_opts.w, window_opts.h, window_opts.sdl_window_flags);
+    if(sdl_w == NULL) {
         Crypt_log_write(__Crypt_2D_renderer_log,
                 "ERROR: Could not open 2D rendering window\nError Message: %s\n",
                 SDL_GetError());
         return FAIL;
     }
+    window->window = sdl_w;
+
+    SDL_Renderer * sdl_r = SDL_CreateRenderer(sdl_w, -1, SDL_RENDERER_ACCELERATED);
+    if((window->renderer = sdl_r) == NULL) {
+        Crypt_log_write(__Crypt_2D_renderer_log,
+                "ERROR: Could not open 2D rendering renderer\nError Message: %s\n",
+                SDL_GetError());
+        goto renderer_create_fail;
+    }
+
+    if((window->surface = SDL_GetWindowSurface(sdl_w)) == NULL) {
+        Crypt_log_write(__Crypt_2D_renderer_log,
+                "ERROR: Could not get 2D surface of window\nError Message: %s\n",
+                SDL_GetError());
+        goto surface_create_fail;
+    }
+
+    SDL_SetRenderDrawColor(sdl_r,
+            window_opts.default_color.rgba_t.r, window_opts.default_color.rgba_t.g,
+            window_opts.default_color.rgba_t.b, window_opts.default_color.rgba_t.a);
+
+    if(SDL_SetRenderDrawBlendMode(sdl_r, SDL_BLENDMODE_BLEND) < 0) {
+        Crypt_log_write(__Crypt_2D_renderer_log,
+                "WARNING: Error on setting alpha blend mode for renderer\nError Message: %s\n",
+                SDL_GetError());
+    }
 
     return SUCCESS;
+
+surface_create_fail:
+    SDL_DestroyRenderer(sdl_r);
+
+renderer_create_fail:
+    SDL_DestroyWindow(sdl_w);
+
+    return FAIL;
 }
