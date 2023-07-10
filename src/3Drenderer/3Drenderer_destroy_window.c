38b0e8c1e59e22fd8faef4e45507880361a4f9e6
Anthony Panarello <aPanarello20@gmail.com>
2023-07-10 14:57:58 -0400

    Destroys an SDL window and clears the Crypt structure's values

---
+++ src/3Drenderer/3Drenderer_destroy_window.c
@@ -0,0 +1,28 @@
+/*  This file is part of Crypt Engine.
+ *
+ *  Crypt Engine is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  Crypt Engine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with Crypt Engine.
+ *  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2023 Anthony Panarello
+ */
+
+#include <Crypt_3D.h>
+#include <Crypt_utils/3Drenderer/internal_type.h>
+
+void Crypt_3D_destroy_window(Crypt_3D_window_t * restrict window) {
+    if(window == NULL || window->sdl_window == NULL) return;
+
+    window->width = window->height = 0;
+    window->camera = NULL;
+    SDL_DestroyRenderer(window->sdl_renderer);
+    SDL_DestroyWindow(window->sdl_window);
+    window->sdl_window = NULL;
+}
