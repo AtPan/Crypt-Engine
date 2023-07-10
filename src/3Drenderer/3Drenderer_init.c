c4527dcc68ed427c71fd8eabaf204c3d919063a4
Anthony Panarello <aPanarello20@gmail.com>
2023-07-10 14:46:52 -0400

    Initializes the 3D rendering module

---
+++ src/3Drenderer/3Drenderer_init.c
@@ -0,0 +1,27 @@
+/*  This file is part of Crypt Engine.
+ *
+ *  Crypt Engine is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  Crypt Engine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with Crypt Engine.
+ *  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2023 Anthony Panarello
+ */
+
+#include <Crypt_3D.h>
+#include <Crypt_utils/3Drenderer/internal_type.h>
+
+flag_t Crypt_3D_init() {
+    int e_code;
+    if((e_code = SDL_Init(SDL_INIT_VIDEO)) < 0) {
+        return FAIL;
+    }
+
+    return SUCCESS;
+}
