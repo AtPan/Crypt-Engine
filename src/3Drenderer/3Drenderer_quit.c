88d7afec3f6c9082849523a4254008238309d0ef
Anthony Panarello <aPanarello20@gmail.com>
2023-07-10 14:55:49 -0400

    Destroys the 3D rendering module

---
+++ src/3Drenderer/3Drenderer_quit.c
@@ -0,0 +1,21 @@
+/*  This file is part of Crypt Engine.
+ *
+ *  Crypt Engine is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  Crypt Engine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with Crypt Engine.
+ *  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2023 Anthony Panarello
+ */
+
+#include <Crypt_3D.h>
+
+void Crypt_3D_quit() {
+    SDL_Quit();
+}
